12ae996a0d9fe9af5e887abc00b2815b3c26a024
Leah McCormick <leahmariemcc3@gmail.com>
2018-05-28 10:32:28 -0400

    lots of style partials created, styles started

--- dev/scripts/InitialUserInput.js
+++ dev/scripts/InitialUserInput.js
@@ -4,19 +4,28 @@ class InitialUserInput extends React.Component {
     render() {
         return(
          <div className="initialUserInput">
+                <img src="public/assets/meteorIcon.png" alt=""/>
              <div className="title">
-                <h1>AstroAtlas</h1>
+                <h1>AstroAtlas.</h1>
+                <p>explore global meteor landings through history</p>
              </div>
-                <form onSubmit={this.props.handleSubmit}>
-                
-                 <select name="year" id="year" onChange={this.props.handleChange} >
-                    {this.props.yearOptions.map( (year, index) => {
-                        return year ? <option value={year} key={index}>{year}</option> : null;
-                    })}
-                 </select>
-                 <label htmlFor="year">Year</label>
-                 <input type="submit" value="Submit"/>
-             </form>
+                <ul className="form">
+                    <form onSubmit={this.props.handleSubmit}>
+                    <li className="styleSelect slate">
+                        <select className="year" name="year" id="year" onChange={this.props.handleChange} >
+                        {this.props.yearOptions.map( (year, index) => {
+                            return year ? <option value={year} key={index}>{year}</option> : null;
+                        })}
+                        </select>
+                    </li>
+                    <li>
+                        <label htmlFor="year">Year</label>
+                    </li>
+                    <li>
+                        <input type="submit" value="Submit"/>
+                    </li>
+                    </form>               
+                </ul>           
          </div>
         )
     }
