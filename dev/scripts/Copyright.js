a73ee54d62ce32f0becfc1d7f949930f898ddd9b
Leah McCormick <leahmariemcc3@gmail.com>
2018-05-29 15:12:37 -0400

    copyright added, modal added and styled

---
+++ dev/scripts/Copyright.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Copyright extends React.Component {
+    render() {
+        return (
+        <div className="copyright">
+                <p>© 2018 by <a href="http://leahmccormick.io" target="_blank">Leah</a>, <a href="http://lindsaypowerharding.com" target="_blank">Lindsay</a>, and <a href="http://wesdevs.com" target="_blank">Wesley</a></p>
+        </div>
+        )
+    }
+}
+
+export default Copyright;
--- dev/scripts/Modal.js
+++ dev/scripts/Modal.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Popup } from "react-mapbox-gl";
 
 class Modal extends React.Component {
     render() {
         return (
-            <div>Hi</div>
+            <div className="box">
+                <button onClick={this.props.function}>X</button> 
+                <h3 className="name">Name</h3> <p>{this.props.name}</p> 
+                <h3>Mass (g)</h3> <p>{this.props.mass}</p>
+                <h3>Class</h3> <p>{this.props.class}</p>
+                <h3>Lat</h3> <p>{this.props.lat}</p>
+                <h3>Long</h3><p>{this.props.long}</p>
+            </div>
         )
     }
 }
